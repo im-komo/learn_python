72f94241bebffec7241fc95e444194094af88af5
im-komo <pravesh.technolabs@gmail.com>
2019-01-06 11:59:05 +0530

    dictionary example

--- test.py
+++ test.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
 
-tuple = ('abcd', 786 , 2.23, 'john', 70.2)  
-tinytuple = (123, 'john')
+dict = {}
+dict['one'] = "This is one"
+dict[2]     = "This is two"
 
-print (tuple)          	# Prints complete list
-print (tuple[0])       	# Prints first element of the list
-print (tuple[1:3])     	# Prints elements starting from 2nd till 3rd 
-print (tuple[2:])      	# Prints elements starting from 3rd element
-print (tinytuple * 2)  	# Prints list two times
-print (tuple + tinytuple) # Prints concatenated lists
-'''
-tuple[2] = 1000    # Invalid syntax with tuple
-list[2] = 1000     # Valid syntax with list
-'''
+tinydict = {'name': 'john','code':6734, 'dept': 'sales'}
+
+print (dict['one'])       # Prints value for 'one' key
+print (dict[2])           # Prints value for 2 key
+print (tinydict)          # Prints complete dictionary
+print (tinydict.keys())   # Prints all the keys
+print (tinydict.values()) # Prints all the values
